1b21014124da88f1aa28c02194d0f06166d80a2e
rodrigues-i <lucas-f250@live.com>
2023-03-21 17:31:26 -0300

    Cria estilo para centralizar os elementos no centro

--- index.php
+++ index.php
@@ -3,34 +3,37 @@
     <head>
         <title>Casos Covid</title>
         <meta charset="utf-8">
+        <link rel="stylesheet" href="./styles/style.css">
     </head>
     <body>
-        <h1>Covid-19 no Mundo</h1>
-        <?php
-            include_once "includes/call_api.php";
-            if(isset($_POST['countries'])) {
-                $selected_country = $_POST['countries'];
-                switch($selected_country) {
-                    case "1":
-                        echo "Brasil foi o país escolhido";
-                        break;
-                    case "2":
-                        echo "Alemanha foi o país escolhido";
-                        break;
-                    case "3":
-                        echo "Russia foi o país escolhido";
-                        break;
+        <main class="main">
+            <h1>Covid-19 no Mundo</h1>
+            <?php
+                include_once "includes/call_api.php";
+                if(isset($_POST['countries'])) {
+                    $selected_country = $_POST['countries'];
+                    switch($selected_country) {
+                        case "1":
+                            echo "Brasil foi o país escolhido";
+                            break;
+                        case "2":
+                            echo "Alemanha foi o país escolhido";
+                            break;
+                        case "3":
+                            echo "Russia foi o país escolhido";
+                            break;
+                    }
                 }
-            }
-        ?>
-        <form method="post" action="" name="form">
-            <select name="countries">
-                <option value="0">Escolhe um País</option>
-                <option value="1">Brasil</option>
-                <option value="2">Alemanha</option>
-                <option value="3">Russia</option>
-            </select>
-            <input name="submit" type="submit">
-        </form>
+            ?>
+            <form method="post" action="" name="form">
+                <select name="countries">
+                    <option value="0">Escolhe um País</option>
+                    <option value="1">Brasil</option>
+                    <option value="2">Alemanha</option>
+                    <option value="3">Russia</option>
+                </select>
+                <input name="submit" type="submit">
+            </form>
+        </main>
     </body>
 </html>

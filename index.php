abfcb95b8b207c6f591e8c873050aa9b587807e9
rodrigues-i <lucas-f250@live.com>
2023-03-21 17:25:12 -0300

    Adiciona um form para selecionar o país desejado

--- index.php
+++ index.php
@@ -8,7 +8,29 @@
         <h1>Covid-19 no Mundo</h1>
         <?php
             include_once "includes/call_api.php";
-            call_api();
+            if(isset($_POST['countries'])) {
+                $selected_country = $_POST['countries'];
+                switch($selected_country) {
+                    case "1":
+                        echo "Brasil foi o país escolhido";
+                        break;
+                    case "2":
+                        echo "Alemanha foi o país escolhido";
+                        break;
+                    case "3":
+                        echo "Russia foi o país escolhido";
+                        break;
+                }
+            }
         ?>
+        <form method="post" action="" name="form">
+            <select name="countries">
+                <option value="0">Escolhe um País</option>
+                <option value="1">Brasil</option>
+                <option value="2">Alemanha</option>
+                <option value="3">Russia</option>
+            </select>
+            <input name="submit" type="submit">
+        </form>
     </body>
 </html>

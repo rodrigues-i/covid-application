d1f879fa57c335417d141f39cf69e29462c05582
rodrigues-i <lucas-f250@live.com>
2023-03-24 15:15:25 -0300

    Corrige erro ortográfico

--- includes/footer.php
+++ includes/footer.php
@@ -3,6 +3,6 @@
         $result = query_latest_search($database);
         $formatted_date = date('d/m/Y', strtotime($result->data));
         $time = date('H:i:s', strtotime($result->data));
-        echo "<p>Último busca: $result->pais em $formatted_date às $time</p>";
+        echo "<p>Última busca: $result->pais em $formatted_date às $time</p>";
     ?>
 </footer>

2331cbb9b68575be18ca02c41d626228aae64a8f
rodrigues-i <lucas-f250@live.com>
2023-03-25 14:59:17 -0300

    Trata caso em que ainda não houve nenhuma busca por país

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,10 @@
 <footer class="footer">
     <?php
         $result = query_latest_search($database);
-        $formatted_date = date('d/m/Y', strtotime($result->data));
-        $time = date('H:i:s', strtotime($result->data));
-        echo "<p>Última busca: $result->pais em $formatted_date às $time</p>";
+        if($result) {
+            $formatted_date = date('d/m/Y', strtotime($result->data));
+            $time = date('H:i:s', strtotime($result->data));
+            echo "<p>Última busca: $result->pais em $formatted_date às $time</p>";
+        }
     ?>
 </footer>
